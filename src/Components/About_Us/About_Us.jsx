87e26e711492df539d77a5633bff5d46e60ccddc
Oplano James Mulbah <mulbahjamesoplano@gmail.com>
2023-12-25 13:43:57 +0200

    Update About_Us.jsx

--- src/Components/About_Us/About_Us.jsx
+++ src/Components/About_Us/About_Us.jsx
@@ -8,14 +8,14 @@ const About_Us = () => {
       <div id={Styles.about_us}>
         <div className={Styles.about_us_wripper}>
           <div className={Styles.about_us_content}>
-            <h1 className={Styles.title}>For developers, by bevalopers</h1>
+            <h1 className={Styles.title}>For developers, by bevalopers.</h1>
             <p className={Styles.description}>
               We are a small, tight-knit team focussed on making Big Cartel
               empowering and easy touse. Some of us run our own Big cartel
               sites. Others are FrontEnd developers, BackEnd developers,
               FullStack developers and App developers. We have work hard to
               build a company while we do big things. We are br' here to help
-              you do the same
+              you do the same.
             </p>
           </div>
           <div className={`${Styles.about_us_content} ${Styles.content_two}`}>
